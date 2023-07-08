6e7bf9a91ec6fb15a5ba276380478821e93afee3
Trimpta <68428753+trimpta@users.noreply.github.com>
2023-07-08 13:59:35 +0530

    Update vectorshit.py

--- vectorshit.py
+++ vectorshit.py
@@ -5,10 +5,12 @@ class vector:
         self.k=k
         self.magnitude=(i*i+j*j+k*k)**0.5
     def __repr__(self):
-        return f"{self.i} {self.j} {self.k}"
+        return f"i:{self.i} j:{self.j} k:{self.k}"
     def __add__(self,other):
         return vector(self.i+other.i,self.j+other.j,self.k+other.k)
     def negative(self):
+        return vector(-self.i,-self.j,-self.k)
+    def __neg__(self):
         return vector(-self.i,-self.j,-self.k)
     def __sub__(self,other):
         return self+other.negative()
@@ -21,3 +23,24 @@ class vector:
             return vector(self.i*other,self.j*other,self.k*other)
     def unit(self):
         return vector(self.i/self.magnitude,self.j/self.magnitude,self.k/self.magnitude)
+    def __abs__(self):
+        return self.magnitude
+    def __call__(self):
+        return((self.i,self.j,self.k))
+    def __truediv__(self,other):
+        return vector(self.i/other,self.j/other,self.k/other)
+    def __eq__(self,other):
+        return (self.i==other.i) and (self.j==other.j) and (self.k==other.k)
+    def __float__(self):
+        return self.magnitude
+    def __gt__(self,other):
+        if isinstance(self,other):
+            return self.magnitude>other.magnitude
+        else:
+            return self.magnitude>abs(other)
+    def __ge__(self,other):
+        if isinstance(self,other):
+            return self.magnitude>=other.magnitude
+        else:
+            return self.magnitude>=abs(other)
+    
